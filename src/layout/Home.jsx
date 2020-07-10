c43176bb47b8ad7d9c7558246863b232c9c7190a
Alina Ilina <alinailina.random@gmail.com>
2020-07-10 15:04:56 +0300

    Fixing mobile issues

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -32,10 +32,10 @@ const Home = () => {
         <h3>Hello</h3>
         <div>
           <p>
-            My name is Alina Ilina. I'm a Web Developer &{" "}
-            <span>UI Imagineer</span>, based in Helsinki, Finland. I make clean,
-            predictable and simply awesome user interfaces for web and mobile
-            apps that meet high standards for usability, and scale.
+            My name is Alina Ilina. I'm a Web Developer{" "}
+            <span>& UI Imagineer</span>, based in Helsinki, Finland. I make
+            clean, predictable and simply awesome user interfaces for web and
+            mobile apps that meet high standards for usability, and scale.
           </p>
           <a href="/about">Read full story</a>
         </div>
